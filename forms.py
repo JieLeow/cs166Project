b8d78c602687e48526d5ee28a86f41dd6529e6d6
JieLeow <lijie.leow@sjsu.edu>
2023-05-10 13:10:07 -0700

    update fields and links

--- forms.py
+++ forms.py
@@ -2,16 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 # from wtforms.fields.html5 import TelField # can be accessed under the wtforms.fields.html5 namespace
 
+from wtforms.fields.html5 import EmailField
 class LoginForm(FlaskForm):
     name = StringField('Name:', id='name')
     website = StringField('Website:', id='website')
     password = PasswordField('Password:', id='password')
 
 class UserRegisterForm(FlaskForm):
-    email = StringField('Email:', id='email')
+    email = EmailField('Email:', id='email')
     password_user = PasswordField('Password:', id='password')
 
 
 class UserLoginForm(FlaskForm):
-    email = StringField('Email:', id='email')
+    email = EmailField('Email:', id='email')
     password_user = PasswordField('Password:', id='password')
