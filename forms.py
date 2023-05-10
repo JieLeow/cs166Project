56bbb859dad0012b9cc54320571f2f099cc7acd3
JieLeow <lijie.leow@sjsu.edu>
2023-05-10 01:13:16 -0700

    add register/login forms

--- forms.py
+++ forms.py
@@ -6,3 +6,12 @@ class LoginForm(FlaskForm):
     name = StringField('Name:', id='name')
     website = StringField('Website:', id='website')
     password = PasswordField('Password:', id='password')
+
+class UserRegisterForm(FlaskForm):
+    email = StringField('Email:', id='email')
+    password_user = PasswordField('Password:', id='password')
+
+
+class UserLoginForm(FlaskForm):
+    email = StringField('Email:', id='email')
+    password_user = PasswordField('Password:', id='password')
